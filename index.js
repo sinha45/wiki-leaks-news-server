47e7ced8a964669aeec710442b0af569f1e9dd28
sinha45 <alfa45leonidas@gmail.com>
2022-12-07 01:07:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res) => {
+    res.send('news api running');
+});
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(port, () => {
+    console.log('news running on port', port);
+});
