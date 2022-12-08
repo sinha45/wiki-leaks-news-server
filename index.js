fe496c5418e6c15a8a4d645a0bd50aabfdbda651
sinha45 <alfa45leonidas@gmail.com>
2022-12-09 01:10:37 +0600

    all done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
     res.send('news api running');
@@ -14,7 +15,36 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === "08") {
+        res.send(news)
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
 })
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+
+
+
+
+app.get('/news/:id', (req, res) => {
+
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+
+
+});
 
 app.listen(port, () => {
     console.log('news running on port', port);
